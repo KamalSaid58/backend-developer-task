410ed476e76466fb8e33c4a8ee3f06e9d92efcde
Kamal Said <kamal.said58@gmail.com>
2026-07-21 00:31:37 +0300

    feat(products) : added validation on update product

--- src/modules/products/dto/update-product.dto.ts
+++ src/modules/products/dto/update-product.dto.ts
@@ -1,6 +1,19 @@
+import * as Joi from 'joi';
+import { JoiSchema } from 'nestjs-joi';
+
 export class UpdateProductDTO {
+  @JoiSchema(Joi.string().uuid().optional())
+  shopId?: string;
+
+  @JoiSchema(Joi.string().trim().min(1).optional())
   name?: string;
+
+  @JoiSchema(Joi.string().trim().min(1).optional())
   description?: string;
+
+  @JoiSchema(Joi.number().integer().min(0).optional())
   price?: number;
+
+  @JoiSchema(Joi.number().integer().min(1).optional())
   stockCount?: number;
 }
